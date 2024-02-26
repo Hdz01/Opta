ba6d79400c2ff72458609a8e9d5d920625761d76
Hdz <bavalos7@ucol.mx>
2024-02-26 13:41:05 -0600

    Update

--- prograrchivo8.py
+++ prograrchivo8.py
@@ -17,3 +17,7 @@ expresion_regular=re.compile(r"\d+(,\d+)*(\.\d+)?")
 resultados_busqueda=expresion_regular.finditer(texto)
 for resultado in resultados_busqueda:
     print(resultado.group(0))
+
+
+
+###afdskjfjdsfjdsfk
